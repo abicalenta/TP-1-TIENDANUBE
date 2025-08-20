4a732f0f1934d29217975100297f72d4ac356091
abigail calenta <abicalenta@gmail.com>
2025-08-20 12:11:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("hola mundo");
+
+let numero1 = 50;
+let numero2 = 70
+
+let string = '50'
+
+let boolean = true
+
+let array = []
+
+let obj = {
+    nombre:'abi',
+    edad: 20,
+    hermano: ['hermano 1', 'hermano 2']
+}
+
+let noDefinido = undefined
+xj
+console.log(numero1 + numero2, string);
+console.log(array[array.length-1])
+console.log(obj.hermano[0])
+
+/** funcion que imprime un saludo */
+function sumar (a,b){
+    let suma = a + b;
+    console.log(suma)
+}
+
+sumar(2,5);
+sumar(1,5);
+sumar(3,5);
+sumar(4,5);
+
+string = 50;
+console.log(string);
